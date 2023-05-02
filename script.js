3f8f0816f5b6093f6d2094e13f2cb96a39d3d0d3
Luiz Felipe Silvano <luizfsilvano@gmail.com>
2023-05-02 16:34:24 -0300

    Melhorias
    Agora, a parte decimal do código é desprezada e o número é formatado separando milhão, mil e etc.
    
    Trabalhando em atualizações para formatação automática do input

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
-  
-    $(function() {
-        $("#my_date_picker1").datepicker({dateFormat: 'dd/mm/yy'});
+    $(function () {
+        $("#my_date_picker1").datepicker({ dateFormat: 'dd/mm/yy' });
     });
 
-    $(function() {
-        $("#my_date_picker2").datepicker({dateFormat: 'dd/mm/yy'});
+    $(function () {
+        $("#my_date_picker2").datepicker({ dateFormat: 'dd/mm/yy' });
+    });
+    $(function () {
+        $("#my_date_picker3").datepicker({ dateFormat: 'dd/mm/yy' });
     });
 
-    $(function(){
-        $("#my_date_picker3").datepicker({dateFormat: 'dd/mm/yy'});
-    });
-
-    $('#my_date_picker1').change(function() {
+    $('#my_date_picker1').change(function () {
         startDate = $(this).datepicker('getDate');
         $("#my_date_picker2").datepicker("option", "minDate", startDate);
     })
 
-    $('#my_date_picker2').change(function() {
+    $('#my_date_picker2').change(function () {
         endDate = $(this).datepicker('getDate');
         $("#my_date_picker1").datepicker("option", "maxDate", endDate);
     })
 
-    $('#my_date_picker3').change(function() {
-        actualDate = $ (this).datepicker('getDate');
+    $('#my_date_picker3').change(function () {
+        actualDate = $(this).datepicker('getDate');
         startDate = $("#my_date_picker1").datepicker('getDate');
         endDate = $("#my_date_picker2").datepicker('getDate');
-        // $("#my_date_picker1").datepicker("option", "", actualDate);
         if (actualDate < startDate) {
             $(this).datepicker("setDate", startDate);
-          } else if (actualDate > endDate) {
+        } else if (actualDate > endDate) {
             $(this).datepicker("setDate", endDate);
-          }
+        }
     })
-})
+});
 
-$('.calcular').on('click', function() {
+function formatNumber(n) {
+    return n.toLocaleString('pt-BR');
+}
+
+$('.calcular').on('click', function () {
     const startDate = $('#my_date_picker1').datepicker('getDate');
     const endDate = $('#my_date_picker2').datepicker('getDate');
     const actualDate = $('#my_date_picker3').datepicker('getDate');
     const contratados = parseInt($('#contratados').val());
     const entregues = parseInt($('#entregues').val());
-    const totalDias = (endDate - startDate) / (1000 * 60 * 60 * 24) +1;
+    const totalDias = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
     const media = contratados / totalDias;
-    const diasPassados = (actualDate - startDate) / (1000 * 60 * 60 * 24) +1;
-    const objetivo = media * diasPassados;
+    const diasPassados = Math.round((actualDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
+    const objetivo = Math.round(media * diasPassados);
     const entreguesAcumulados = entregues - objetivo;
     let textarea = document.getElementById('email');
     let modeloEmail = `Boa tarde a todos!, \n\nVerificamos em nosso dashboard que as entregas estão abaixo do contratado. Poderiam, por gentileza, verificar? \n\nQualquer dúvida estou a disposição,\nAbs.`;
-    
-    if (objetivo > entregues) {
-        alert(`Você está abaixo da média de entregas. Média: ${media}, Objetivo ${objetivo}`);
-        textarea.innerHTML = modeloEmail;
 
+    if (objetivo > entregues) {
+        alert(`Você está abaixo da média de entregas. Média: ${formatNumber(media)}, Objetivo: ${formatNumber(objetivo)}`);
+        textarea.innerHTML = modeloEmail;
     } else {
-        alert(`Você está acima da média de entregas. Média: ${media}, Objetivo: ${objetivo}`);
+        alert(`Você está acima da média de entregas. Média: ${formatNumber(media)}, Objetivo: ${formatNumber(objetivo)}`);
     }
-    // $('#my_date_picker1').val('');
-    // $('#my_date_picker2').val('');
     $('#contratados').val('');
     $('#entregues').val('');
 });
@@ -66,21 +64,16 @@ $('.calcular').on('click', function() {
 async function clipboardCopy() {
     let text = document.querySelector("#email").value;
     await navigator.clipboard.writeText(text);
-  }
-  
-  document.getElementById('copy').addEventListener('click', clipboardCopy);
-  
+}
 
-
-
+document.getElementById('copy').addEventListener('click', clipboardCopy);
 
 const lightmode = document.getElementById('button');
 const card = document.getElementById('card');
 const bg = document.getElementById("black");
-lightmode.addEventListener('click', function() {
+lightmode.addEventListener('click', function () {
     console.log("Foi pressionado!");
     card.classList.toggle("light-mode");
+    bg.classList.toggle("bg-light");
 });
-lightmode.addEventListener('click', function() {
-    bg.classList.toggle("dark-mode");
-});
+
